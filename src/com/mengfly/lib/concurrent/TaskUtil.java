a51835e5172f8d7da4dc878f348321242ba48985
wangpengfei <www.wangpengfei99@gmail.com>
2019-10-29 15:25:51 +0800

    add Class DateUtil and perfect method for TaskUtil

--- src/com/mengfly/lib/concurrent/TaskUtil.java
+++ src/com/mengfly/lib/concurrent/TaskUtil.java
@@ -1,4 +1,4 @@
-package concurrent;
+package com.mengfly.lib.concurrent;
 
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
@@ -7,6 +7,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.DoubleConsumer;
 import java.util.function.ObjIntConsumer;
 
 import com.mengfly.lib.CollectionUtil;
@@ -33,7 +34,8 @@ public class TaskUtil {
 	 * @param childTaskReses 子任务要处理的数据列表
 	 * @param childTaskLogic 每个子任务的处理逻辑
 	 */
-	public <E> void splitTaskExec(List<List<E>> childTaskReses, ObjIntConsumer<List<E>> childTaskLogic) {
+	public <E> void splitTaskExec(List<List<E>> childTaskReses, ObjIntConsumer<List<E>> childTaskLogic,
+			ObjIntConsumer<Exception> errorHandler, DoubleConsumer progressHandler) {
 		int taskCount = childTaskReses.size();
 		CountDownLatch begin = new CountDownLatch(1);
 		CountDownLatch end = new CountDownLatch(taskCount);
@@ -47,8 +49,14 @@ public class TaskUtil {
 					childTaskLogic.accept(childTaskRe, location);
 				} catch (InterruptedException e) {
 					System.err.println("子任务执行异常：" + Thread.currentThread().getName() + "->" + e.getMessage());
+					if (errorHandler != null) {
+						errorHandler.accept(e, location);
+					}
 				} finally {
 					end.countDown();
+					if (progressHandler != null) {
+						progressHandler.accept((taskCount - end.getCount()) * 1.0 / taskCount);
+					}
 				}
 			};
 			getExecutor().submit(runnable);
@@ -70,7 +78,20 @@ public class TaskUtil {
 	 */
 	public <E> void splitTaskExec(List<E> res, int childTaskItemNum, ObjIntConsumer<List<E>> childTaskLogic) {
 		List<List<E>> childTaskRes = CollectionUtil.split(res, childTaskItemNum);
-		splitTaskExec(childTaskRes, childTaskLogic);
+		splitTaskExec(childTaskRes, childTaskLogic, null, null);
+	}
+
+	/**
+	 * 将任务拆分为子任务使用线程进行执行，加快任务分析速度
+	 * 
+	 * @param res              要拆分的总任务
+	 * @param childTaskItemNum 每个子任务中的item数量
+	 * @param childTaskLogic   每个子任务的处理逻辑
+	 */
+	public <E> void splitTaskExec(List<E> res, int childTaskItemNum, ObjIntConsumer<List<E>> childTaskLogic,
+			ObjIntConsumer<Exception> exceptionHandler, DoubleConsumer processHandler) {
+		List<List<E>> childTaskRes = CollectionUtil.split(res, childTaskItemNum);
+		splitTaskExec(childTaskRes, childTaskLogic, exceptionHandler, processHandler);
 	}
 
 	public static TaskUtil getInstance() {

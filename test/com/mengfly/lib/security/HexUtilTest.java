312a0235749d07ccbc5ed5f04d3244019cb7c3d4
wangpengfei <www.wangpengfei99@gmail.com>
2019-11-22 18:39:55 +0800

    add int2bin and change hex lib name

---
+++ test/com/mengfly/lib/security/HexUtilTest.java
@@ -0,0 +1,24 @@
+package com.mengfly.lib.security;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HexUtilTest {
+
+	/*
+	 * Test Case:
+	 * 
+	 * YL001：59 4C 30 30 31
+	 * MG255：4D 47 32 35 35 
+	 * LC001：4C 43 30 30 31
+	 */
+	@Test
+	void testStr2ASCII2Hex() {
+		assertEquals(HexBinUtil.str2ASCII2Hex("YL001"), "59 4C 30 30 31");
+		assertEquals(HexBinUtil.str2ASCII2Hex("MG255"), "4D 47 32 35 35");
+		assertEquals(HexBinUtil.str2ASCII2Hex("LC001"), "4C 43 30 30 31");
+
+	}
+
+}
